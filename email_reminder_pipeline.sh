f9d04359dc8084f25a4fa6243a6c80b79391215e
Jacob Wheeler <dukob86@yahoo.com>
2026-04-20 00:11:16 -0400

    feat: remove file

--- email_reminder_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cat test_snapshot.csv \
-  | python mappers/sort-by-month.py \
-  | python mappers/speaker-name-mapper.py \
-  | python mappers/unit-name-mapper.py \
-  > final_schedule.csv
